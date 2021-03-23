e5e38d715ea979778b6b4b69d075b7a6b21bd816
Peter Polman <peter@peterpolman.nl>
2021-03-23 14:11:31 +0100

    Adds check for withdrawal balance subtracts

--- contracts/05-Withdraw/WithdrawPoll.sol
+++ contracts/05-Withdraw/WithdrawPoll.sol
@@ -2,22 +2,22 @@
 pragma solidity ^0.7.0;
 pragma experimental ABIEncoderV2;
 
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/EnumerableSet.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
-import "diamond-2/contracts/libraries/LibDiamond.sol";
+import '@openzeppelin/contracts/token/ERC20/SafeERC20.sol';
+import '@openzeppelin/contracts/math/SafeMath.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/utils/EnumerableSet.sol';
+import '@openzeppelin/contracts/utils/Address.sol';
+import 'diamond-2/contracts/libraries/LibDiamond.sol';
 
 // Implements
-import "../util/BasePoll.sol"; // TMP1, TMP 6
-import "../TMP/TMP6/LibBasePollStorage.sol";
-import "../TMP/TMP7/IWithdrawPoll.sol";
-import "../TMP/TMP7/LibWithdrawPollStorage.sol";
+import '../util/BasePoll.sol'; // TMP1, TMP 6
+import '../TMP/TMP6/LibBasePollStorage.sol';
+import '../TMP/TMP7/IWithdrawPoll.sol';
+import '../TMP/TMP7/LibWithdrawPollStorage.sol';
 
 // depends on
-import "../TMP/TMP5/LibTokenStorage.sol";
-import "../TMP/TMP2/LibMemberAccessStorage.sol";
+import '../TMP/TMP5/LibTokenStorage.sol';
+import '../TMP/TMP2/LibMemberAccessStorage.sol';
 
 contract WithdrawPoll is BasePoll, IWithdrawPoll {
     using SafeERC20 for IERC20;
@@ -26,11 +26,10 @@ contract WithdrawPoll is BasePoll, IWithdrawPoll {
     modifier isWithdraw {
         LibBasePollStorage.BasePollStorage storage bData = baseData();
 
+        LibWithdrawPollStorage.WithdrawPollStorage storage wpPollData =
+            LibWithdrawPollStorage.withdrawPollStorageId(bData.id);
 
-            LibWithdrawPollStorage.WithdrawPollStorage storage wpPollData
-         = LibWithdrawPollStorage.withdrawPollStorageId(bData.id);
-
-        require(wpPollData.beneficiary != 0, "NOT_WITHDRAW_POLL");
+        require(wpPollData.beneficiary != 0, 'NOT_WITHDRAW_POLL');
         _;
     }
 
@@ -40,18 +39,19 @@ contract WithdrawPoll is BasePoll, IWithdrawPoll {
     function onPollFinish(uint256 _id) internal override {
         bool approved = _withdrawPollApprovalState();
 
-
-            LibWithdrawPollStorage.WithdrawPollStorage storage wpPollData
-         = LibWithdrawPollStorage.withdrawPollStorageId(_id);
+        LibWithdrawPollStorage.WithdrawPollStorage storage wpPollData =
+            LibWithdrawPollStorage.withdrawPollStorageId(_id);
 
         if (approved) {
-            LibTokenStorage.TokenStorage storage s = LibTokenStorage
-                .tokenStorage();
-            s.balance = s.balance.sub(wpPollData.amount);
+            LibTokenStorage.TokenStorage storage s = LibTokenStorage.tokenStorage();
 
-            address benef = LibMemberAccessStorage
-                .memberStorage()
-                .memberToAddress[wpPollData.beneficiary];
+            if (s.balance >= wpPollData.amount) {
+                s.balance = s.balance.sub(wpPollData.amount);
+            } else {
+                s.balance = 0;
+            }
+
+            address benef = LibMemberAccessStorage.memberStorage().memberToAddress[wpPollData.beneficiary];
             s.token.safeTransfer(benef, wpPollData.amount);
             emit Withdrawn(_id, benef, wpPollData.amount);
         }
@@ -62,19 +62,14 @@ contract WithdrawPoll is BasePoll, IWithdrawPoll {
     }
 
     function voteValidate(bool _agree, address _voter) internal override {
-        require(_isManager(_voter), "NO_MANAGER");
+        require(_isManager(_voter), 'NO_MANAGER');
     }
 
-    function getBeneficiary(uint256 _id)
-        public
-        override
-        view
-        returns (uint256)
-    {
+    function getBeneficiary(uint256 _id) public view override returns (uint256) {
         return LibWithdrawPollStorage.withdrawPollStorageId(_id).beneficiary;
     }
 
-    function getAmount(uint256 _id) public override view returns (uint256) {
+    function getAmount(uint256 _id) public view override returns (uint256) {
         return LibWithdrawPollStorage.withdrawPollStorageId(_id).amount;
     }
 
@@ -92,14 +87,7 @@ contract WithdrawPoll is BasePoll, IWithdrawPoll {
         finalize();
     }
 
-    function _withdrawPollApprovalState()
-        public
-        virtual
-        override
-        view
-        isWithdraw
-        returns (bool)
-    {
+    function _withdrawPollApprovalState() public view virtual override isWithdraw returns (bool) {
         LibBasePollStorage.BasePollStorage storage bData = baseData();
         return bData.yesCounter > bData.noCounter;
     }
