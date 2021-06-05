327b9a86c201d8f7e83bd3505f39ed6617ea63d6
Evert Kors <evert@hatching.io>
2021-06-05 11:34:28 +0200

    Prettier

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,21 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-etherscan");
-require("@nomiclabs/hardhat-web3");
-require("dotenv").config();
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-etherscan');
+require('@nomiclabs/hardhat-web3');
+require('dotenv').config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
-const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY || "";
-const ETHERSCAN_API = process.env.ETHERSCAN_API || "";
+const INFURA_API_KEY = process.env.INFURA_API_KEY || '';
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY || '';
+const ETHERSCAN_API = process.env.ETHERSCAN_API || '';
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
+task('accounts', 'Prints the list of accounts', async () => {
+    const accounts = await ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+    for (const account of accounts) {
+        console.log(account.address);
+    }
 });
 
 // You need to export an object to set up your config
@@ -25,24 +25,22 @@ task("accounts", "Prints the list of accounts", async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.7.4",
-  networks: {
-    goerli: {
-      url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [GOERLI_PRIVATE_KEY].filter((item) => item !== ""),
-      timeout: 2483647,
+    solidity: '0.7.4',
+    networks: {
+        goerli: {
+            url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
+            accounts: [GOERLI_PRIVATE_KEY].filter((item) => item !== ''),
+            timeout: 2483647,
+        },
+        fork: {
+            url: `http://127.0.0.1:8545/`,
+            accounts: ['eea0247bd059ac4d2528adb36bb0de003d62ba568e3197984b61c41d9a132df0'],
+            timeout: 2483647,
+        },
     },
-    fork: {
-      url: `http://127.0.0.1:8545/`,
-      accounts: [
-        "eea0247bd059ac4d2528adb36bb0de003d62ba568e3197984b61c41d9a132df0",
-      ],
-      timeout: 2483647,
+    etherscan: {
+        // Your API key for Etherscan
+        // Obtain one at https://etherscan.io/
+        apiKey: ETHERSCAN_API,
     },
-  },
-  etherscan: {
-    // Your API key for Etherscan
-    // Obtain one at https://etherscan.io/
-    apiKey: ETHERSCAN_API,
-  },
 };
