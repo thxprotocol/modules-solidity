8096c18b7c8d0302999b79a8492f36b7738f9f5a
Bram Rongen <bram@thx.network>
2022-03-15 15:20:12 +0100

    Deploy example token with limited supply

---
+++ deploy/004_example_token.ts
@@ -0,0 +1,22 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { parseUnits } from 'ethers/lib/utils';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    const { deployments, getNamedAccounts } = hre;
+    const { deploy } = deployments;
+
+    const { deployer } = await getNamedAccounts();
+
+    await deploy('TokenLimitedSupply', {
+        from: deployer,
+        args: ['THX Token', 'THX', deployer, parseUnits('1000000', 'ether')],
+        log: true,
+        autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
+    });
+
+    return hre.network.live; // Makes sure we don't redeploy on live networks
+};
+export default func;
+func.id = '004_example_token';
+func.tags = ['TokenLimitedSupply'];
