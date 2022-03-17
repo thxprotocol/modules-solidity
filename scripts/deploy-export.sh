09374a000e4396c2bb74ab5c613691677d1953aa
Bram Rongen <bram@thx.network>
2022-03-17 10:17:58 +0100

    Split deploy-export to deploy-export and version scripts.
    This makes it easier to re-run deploy-export multiple times

--- scripts/deploy-export.sh
+++ scripts/deploy-export.sh
@@ -2,8 +2,6 @@
 # Exit on any errors.
 set -e
 
-npm version --no-git-tag --silent ${1:-patch} 
-
 VERSION=`echo $(node -e "console.log(require('./package.json').version);")`
 
 # Store latest version for hardhat network.
@@ -16,13 +14,4 @@ do
     echo "Deploying to $NETWORK:"
     npx hardhat deploy --network $NETWORK --export exports/$NETWORK/$VERSION.json
 done
-
-git add . 
-git commit -m"Publish: Deployed and exported version $VERSION"
-
-TAG=v$VERSION
-git tag $TAG
-git push $TAG
-
-echo "Ready to package and push to NPM"
 
