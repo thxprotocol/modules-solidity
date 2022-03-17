7f02a2b56955c63fb5890c3db938df6ffb0ad7ba
Bram Rongen <bram@thx.network>
2022-03-17 12:25:47 +0100

    Allow skipping of npm version and add npm publish

---
+++ scripts/publish.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Exit on any errors.
+set -e
+
+BUMP=${1:-patch} 
+if [$BUMP != "none"]
+then
+    npm version --no-git-tag --silent $BUMP
+fi
+
+./deploy-export.sh
+
+git add . 
+git commit --allow-empty -m"Publish: Deployed and exported version $VERSION"
+
+TAG=v$VERSION
+git tag $TAG
+git push $TAG
+
+npm run build
+npm run compile
+npm publish
