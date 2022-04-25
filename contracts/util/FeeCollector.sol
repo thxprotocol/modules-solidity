8d6dc35eb5474dd0fb80561aa93b6cbd99202e5f
wooyh <yung.hui.woo@hva.nl>
2022-04-25 16:46:23 +0200

    Code cleanup

--- contracts/util/FeeCollector.sol
+++ contracts/util/FeeCollector.sol
@@ -4,14 +4,10 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract FeeCollector is Ownable, ERC20  {
-    address public owner;
-
     event TransferReceived(address _from, uint _amount);
     event TransferSent(address _from, address _destAddr, uint _amount);
 
-    constructor(){
-        owner = msg.sender;
-    }
+    constructor(){}
 
     function transferERC20(IERC20 token, address to, uint256 amount) public onlyOwner {
         uint256 erc20balance = token.balanceOf(address(this));
@@ -19,4 +15,5 @@ contract FeeCollector is Ownable, ERC20  {
         token.transfer(to, amount);
         emit TransferSent(msg.sender, to, amount);
     }
+
 }
--- contracts/util/TrackReward.sol
+++ contracts/util/TrackReward.sol
@@ -1,19 +1,10 @@
 pragma solidity ^0.8.0;
-import '@openzeppelin/contracts/access/Ownable.sol';
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
-contract RewardPayout is Ownable, ERC20 {
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+contract TrackReward is Ownable {
     mapping(address => uint256) public rewards;
     constructor(){}
-
-    /*
-     * @dev Transfers reward - estimatedGasCost to the user if the transaction fees are lower then the reward.
-     */
-    function transferreward()  payable public onlyOwner {
-
-    }
-
-
 
     /*
      * @dev Store new reward in the local reward variable
@@ -29,9 +20,4 @@ contract RewardPayout is Ownable, ERC20 {
     function getReward() public onlyOwner {
         return rewards[msg.sender];
     }
-
-    /*
-     * @dev Function to receive tokens on this contract.
-     */
-    fallback() external payable {}
 }
