019d1b13e448d63a5e1d7b4f3ae56a9f50317ffc
Mert <mert.gokseli@hva.nl>
2022-05-03 15:10:44 +0200

    Latest

--- contracts/04-Token/StTHX.sol
+++ contracts/04-Token/StTHX.sol
@@ -1,18 +1,16 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DevToken is ERC20, Ownable {
-
     constructor(
         string memory name_,
         string memory symbol_,
         address owner_
     ) ERC20(name_, symbol_) {
         transferOwnership(owner_);
-        
     }
 
     function mint(address receiver, uint256 amount) external onlyOwner returns (uint256) {
