6b8b23fd19fbd6fb52ff18f809bf39ea810750e2
Danieldvaj <daniel.van.apeldoorn@hva.nl>
2022-05-04 18:45:24 +0200

    added allocation

---
+++ contracts/13-Allocation/Allocation.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.4;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+contract allocation {
+
+
+    mapping(address => mapping(address => uint)) public allocations;
+
+//Allocating per user the coin and the amount it has    
+    using SafeMath for uint;
+    function allocate(address _tokenAddress, uint allocating) public {
+        allocations[_tokenAddress][msg.sender] = allocations[_tokenAddress][msg.sender].add(allocating);
+    }
+
+//delete test for remix
+    function payout(address _tokenAddress) public {
+         delete allocations[_tokenAddress][msg.sender];
+    }
+
+//show test voor remix
+    function showAllocation(address _tokenAddress) public view returns (uint256) {
+         return allocations[_tokenAddress][msg.sender];
+    
+    }
+     
+
+
+
+}
+
+
+  
