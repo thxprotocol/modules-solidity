cd675351ece4ffa1962712b5d42a4348e9941190
Peter Polman <peter@peterpolman.nl>
2022-05-05 15:03:48 +0200

    Remove console.log

--- contracts/util/ERC20/UnlimitedSupplyToken.sol
+++ contracts/util/ERC20/UnlimitedSupplyToken.sol
@@ -13,8 +13,6 @@ import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-import 'hardhat/console.sol';
-
 contract UnlimitedSupplyToken is ERC20, AccessControl, Ownable {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     
