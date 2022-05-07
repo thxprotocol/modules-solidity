679ec2fd21ae4874594e4c9c078717870436e166
AlmarMG <almar.gemmel@gmail.com>
2022-05-07 16:10:21 +0200

    indentation gefixt allocation file

--- contracts/13-Allocation/Allocation.sol
+++ contracts/13-Allocation/Allocation.sol
@@ -6,29 +6,21 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 contract allocation {
 
 
-    mapping(address => mapping(address => uint)) public allocations;
+  mapping(address => mapping(address => uint)) public allocations;
 
-//Allocating per user the coin and the amount it has    
-    using SafeMath for uint;
-    function allocate(address _tokenAddress, uint allocating) public {
-        allocations[_tokenAddress][msg.sender] = allocations[_tokenAddress][msg.sender].add(allocating);
-    }
+  //Allocating per user the coin and the amount it has    
+  using SafeMath for uint;
+  function allocate(address _tokenAddress, uint allocating) public {
+    allocations[_tokenAddress][msg.sender] = allocations[_tokenAddress][msg.sender].add(allocating);
+  }
 
-//delete test for remix
-    function payout(address _tokenAddress) public {
-         delete allocations[_tokenAddress][msg.sender];
-    }
+  //delete test for remix
+  function payout(address _tokenAddress) public {
+    delete allocations[_tokenAddress][msg.sender];
+  }
 
-//show test voor remix
-    function showAllocation(address _tokenAddress) public view returns (uint256) {
-         return allocations[_tokenAddress][msg.sender];
-    
-    }
-     
-
-
-
+  //show test voor remix
+  function showAllocation(address _tokenAddress) public view returns (uint256) {
+    return allocations[_tokenAddress][msg.sender];
+  }
 }
-
-
-  
