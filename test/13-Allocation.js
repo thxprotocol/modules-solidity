e252d50a9054ae9e83dd5c4ee6b85b1781cbc8b5
Danieldvaj <daniel.van.apeldoorn@hva.nl>
2022-05-08 15:34:33 +0200

    gedeelte testfunctie 2

--- test/13-Allocation.js
+++ test/13-Allocation.js
@@ -3,10 +3,16 @@ const e = require('express');
 const { ethers } = require('hardhat');
 const { isCallTrace } = require('hardhat/internal/hardhat-network/stack-traces/message-trace');
 
-describe.only ('Allocation', function () {
-    const testalloc = await ethers.getContractFactory('Allocation');
+describe.only ('alloctest', function () {
+
+    before(async function() {
+        const testalloc = await ethers.getContractFactory('allocation');
+
+        alloctest = await testalloc.deploy();
+        await alloctest.deployed();
+    });
 
     it('Should be allocate coin and amount and also check if its an address and integer', async function () {
-        await testalloc.allocate(0x71C7656EC7ab88b098defB751B7401B5f6d8976F, 1000);
+        await alloctest.allocate(0xcd3b766ccdd6ae721141f452c550ca635964ce71, 1000);
     });
 });
