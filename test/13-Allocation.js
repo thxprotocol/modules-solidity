28f3060d004dfe2a9de597e79badb5ceb8870b1d
Danieldvaj <daniel.van.apeldoorn@hva.nl>
2022-05-08 15:42:12 +0200

    gedeelte testfunctie 3

--- test/13-Allocation.js
+++ test/13-Allocation.js
@@ -13,6 +13,6 @@ describe.only ('alloctest', function () {
     });
 
     it('Should be allocate coin and amount and also check if its an address and integer', async function () {
-        await alloctest.allocate(0xcd3b766ccdd6ae721141f452c550ca635964ce71, 1000);
+        await alloctest.allocate("0xcd3b766ccdd6ae721141f452c550ca635964ce71", 1000);
     });
 });
