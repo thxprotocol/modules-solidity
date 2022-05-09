e5c6406fd13c44aa9a6a429293eab87423e2fca4
Danieldvaj <daniel.van.apeldoorn@hva.nl>
2022-05-09 11:22:01 +0200

    testfunctie compleet

--- test/13-Allocation.js
+++ test/13-Allocation.js
@@ -6,6 +6,7 @@ const { isCallTrace } = require('hardhat/internal/hardhat-network/stack-traces/m
 describe.only('alloctest', function () {
 
     before(async function() {
+        //const [user] = await ethers.getSigners();
         const testalloc = await ethers.getContractFactory('allocation');
 
         alloctest = await testalloc.deploy();
@@ -13,15 +14,16 @@ describe.only('alloctest', function () {
     });
 
     it('Should be allocate coin and amount and also check if its an address and integer', async function () {
-        await expect(alloctest.allocate(0x71C7656EC7ab88b098defB751B7401B5f6d8976F, 1000));
+        await expect(alloctest.allocate("0x71C7656EC7ab88b098defB751B7401B5f6d8976F", 1000));
     });
     it('Show the availabe amount per', async function () {
-        let test = alloctest.showAllocation(0x71C7656EC7ab88b098defB751B7401B5f6d8976F);
+        let test = alloctest.showAllocation("0x71C7656EC7ab88b098defB751B7401B5f6d8976F");
+        console.log("Running test");
         test.then(function(result) {
             if (result == 1000){
-                console.log("yes");
+                console.log("Coin en amount komt overeen");
             } else {
-                console.log("no");
+                console.log("Coin en amount komt niet overeen");
             }
         })
     });
