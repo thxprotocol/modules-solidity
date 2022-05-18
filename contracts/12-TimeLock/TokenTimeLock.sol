fc7c5336b28b560aec5b0ebef5d00d95b965adb9
AlmarMG <almar.gemmel@gmail.com>
2022-05-18 15:39:06 +0200

    indentation fixed

--- contracts/12-TimeLock/TokenTimeLock.sol
+++ contracts/12-TimeLock/TokenTimeLock.sol
@@ -11,124 +11,124 @@ import '../util/ERC20/IUnlimitedSupplyToken.sol';
 // one can deposit into this contract but you must wait 1 week before you can withdraw your funds
 
 contract TokenTimeLock {
-    using SafeMath for uint256;
-    // amount you deposited is saved in balances
-    mapping(address => uint256) public balances;
-    // when you can withdraw is saved in lockTime
-    mapping(address => uint256) public lockTime;
+  using SafeMath for uint256;
+  // amount you deposited is saved in balances
+  mapping(address => uint256) public balances;
+  // when you can withdraw is saved in lockTime
+  mapping(address => uint256) public lockTime;
 
-    mapping(address => mapping(address => uint256)) public allocations;
+  mapping(address => mapping(address => uint256)) public allocations;
 
-    address admin;
-    address[] public addresses;
+  address admin;
+  address[] public addresses;
 
-    IERC20 private THXtoken;
-    IUnlimitedSupplyToken stTHXtoken;
-    IERC20 private RewardToken1;
-    IERC20 private RewardToken2;
-    IERC20 private RewardToken3;
-    IERC20 private RewardToken4;
+  IERC20 private THXtoken;
+  IUnlimitedSupplyToken stTHXtoken;
+  IERC20 private RewardToken1;
+  IERC20 private RewardToken2;
+  IERC20 private RewardToken3;
+  IERC20 private RewardToken4;
 
-    constructor(address _stTHXtoken, address _THXtoken) public {
-        THXtoken = IERC20(_THXtoken);
-        stTHXtoken = IUnlimitedSupplyToken(_stTHXtoken);
-        admin = msg.sender;
-    }
+  constructor(address _stTHXtoken, address _THXtoken) public {
+    THXtoken = IERC20(_THXtoken);
+    stTHXtoken = IUnlimitedSupplyToken(_stTHXtoken);
+    admin = msg.sender;
+  }
 
-    modifier onlyAdmin() {
-        require(msg.sender == admin, 'ADMIN_ONLY');
-        _;
-    }
+  modifier onlyAdmin() {
+    require(msg.sender == admin, "ADMIN_ONLY");
+    _;
+  }
 
-    function addexampleTokens(
-        address _RewardToken1,
-        address _RewardToken2,
-        address _RewardToken3,
-        address _RewardToken4
-    ) public {
-        RewardToken1 = IERC20(_RewardToken1);
-        RewardToken2 = IERC20(_RewardToken2);
-        RewardToken3 = IERC20(_RewardToken3);
-        RewardToken4 = IERC20(_RewardToken4);
-    }
+  function addexampleTokens(
+    address _RewardToken1,
+    address _RewardToken2,
+    address _RewardToken3,
+    address _RewardToken4
+  ) public {
+    RewardToken1 = IERC20(_RewardToken1);
+    RewardToken2 = IERC20(_RewardToken2);
+    RewardToken3 = IERC20(_RewardToken3);
+    RewardToken4 = IERC20(_RewardToken4);
+  }
 
-    function deposit(uint256 amount, uint256 _increase) external payable {
-        require(amount >= 10, 'Cannot stake less than 10');
-        // update total staked
-        balances[msg.sender] = balances[msg.sender].add(amount);
-        addresses.push(msg.sender);
-        // Omrekenen tijd in weken naar seconden
-        _increase = _increase.mul(604800);
-        // updated locktime 1 week from now
-        lockTime[msg.sender] = block.timestamp.add(_increase);
-        // Transfer THX naar contract voor staken
-        THXtoken.transferFrom(msg.sender, address(this), amount);
-        // Transfer stTHX naar User
-        stTHXtoken.approve(address(this), amount);
-        stTHXtoken.transferFrom(address(this), msg.sender, amount);
+  function deposit(uint256 amount, uint256 _increase) external payable {
+    require(amount >= 10, "Cannot stake less than 10");
+    // update total staked
+    balances[msg.sender] = balances[msg.sender].add(amount);
+    addresses.push(msg.sender);
+    // Omrekenen tijd in weken naar seconden
+    _increase = _increase.mul(604800);
+    // updated locktime 1 week from now
+    lockTime[msg.sender] = block.timestamp.add(_increase);
+    // Transfer THX naar contract voor staken
+    THXtoken.transferFrom(msg.sender, address(this), amount);
+    // Transfer stTHX naar User
+    stTHXtoken.approve(address(this), amount);
+    stTHXtoken.transferFrom(address(this), msg.sender, amount);
 
-        // Log hoeveel gestaked is
-        emit Staked(msg.sender, amount);
-    }
+    // Log hoeveel gestaked is
+    emit Staked(msg.sender, amount);
+  }
 
-    function amountStaked() public view returns (uint256) {
-        return balances[msg.sender];
-    }
+  function amountStaked() public view returns (uint256) {
+    return balances[msg.sender];
+  }
 
-    function getAddress() public view returns (address[] memory) {
-        return addresses;
-    }
+  function getAddress() public view returns (address[] memory) {
+    return addresses;
+  }
 
     // Allocating per user the coin and the amount it has
-    function allocate(
-        address _userAddress,
-        address _tokenAddress,
-        uint256 allocating
-    ) public onlyAdmin {
-        allocations[_tokenAddress][_userAddress] = allocations[_tokenAddress][_userAddress].add(allocating);
-        emit Allocated(_tokenAddress, allocating);
-    }
+  function allocate(
+    address _userAddress,
+    address _tokenAddress,
+    uint256 allocating
+  ) public onlyAdmin {
+    allocations[_tokenAddress][_userAddress] = allocations[_tokenAddress][_userAddress].add(allocating);
+    emit Allocated(_tokenAddress, allocating);
+  }
 
-    // delete test for remix
-    function payout(address _tokenAddress) public {
-        delete allocations[_tokenAddress][msg.sender];
-    }
+  // delete test for remix
+  function payout(address _tokenAddress) public {
+    delete allocations[_tokenAddress][msg.sender];
+  }
 
     // show test voor remix
-    function showAllocation(address _tokenAddress) public view returns (uint256) {
-        return allocations[_tokenAddress][msg.sender];
-    }
+  function showAllocation(address _tokenAddress) public view returns (uint256) {
+    return allocations[_tokenAddress][msg.sender];
+  }
 
-    function withdraw(address _tokenAddress) public {
-        // check if that the sender has deposited in this contract in the mapping and the balance >0
-        require(balances[msg.sender] > 0, 'There is no funds added');
-        // check that the now time is > the time saved in the lock time mapping
-        require(block.timestamp > lockTime[msg.sender], 'lock time has not expired yet');
+  function withdraw(address _tokenAddress) public {
+    // check if that the sender has deposited in this contract in the mapping and the balance >0
+    require(balances[msg.sender] > 0, "There is no funds added");
+    // check that the now time is > the time saved in the lock time mapping
+    require(block.timestamp > lockTime[msg.sender], "lock time has not expired yet");
 
-        // update balance
-        uint256 amount = balances[msg.sender];
-        delete balances[msg.sender];
-        // burn staked thx
-        stTHXtoken.burn(msg.sender, amount);
-        // send the money to the sender
-        THXtoken.approve(address(this), amount);
-        THXtoken.transferFrom(address(this), msg.sender, amount);
+    // update balance
+    uint256 amount = balances[msg.sender];
+    delete balances[msg.sender];
+    // burn staked thx
+    stTHXtoken.burn(msg.sender, amount);
+    // send the money to the sender
+    THXtoken.approve(address(this), amount);
+    THXtoken.transferFrom(address(this), msg.sender, amount);
 
-        RewardToken1 = IERC20(_tokenAddress);
-        RewardToken1.transferFrom(address(this), msg.sender, allocations[_tokenAddress][msg.sender]);
-        RewardToken2 = IERC20(_tokenAddress);
-        RewardToken2.transferFrom(address(this), msg.sender, allocations[_tokenAddress][msg.sender]);
-        RewardToken3 = IERC20(_tokenAddress);
-        RewardToken3.transferFrom(address(this), msg.sender, allocations[_tokenAddress][msg.sender]);
-        RewardToken4 = IERC20(_tokenAddress);
-        RewardToken4.transferFrom(address(this), msg.sender, allocations[_tokenAddress][msg.sender]);
+    RewardToken1 = IERC20(_tokenAddress);
+    RewardToken1.transferFrom(address(this), msg.sender, allocations[_tokenAddress][msg.sender]);
+    RewardToken2 = IERC20(_tokenAddress);
+    RewardToken2.transferFrom(address(this), msg.sender, allocations[_tokenAddress][msg.sender]);
+    RewardToken3 = IERC20(_tokenAddress);
+    RewardToken3.transferFrom(address(this), msg.sender, allocations[_tokenAddress][msg.sender]);
+    RewardToken4 = IERC20(_tokenAddress);
+    RewardToken4.transferFrom(address(this), msg.sender, allocations[_tokenAddress][msg.sender]);
 
-        // delete allocated rewards
-        payout(_tokenAddress);
-        emit Withdrawn(msg.sender, amount);
-    }
+    // delete allocated rewards
+    payout(_tokenAddress);
+    emit Withdrawn(msg.sender, amount);
+  }
 
-    event Allocated(address _tokenAddress, uint256 _allocating);
-    event Staked(address indexed user, uint256 amount);
-    event Withdrawn(address indexed user, uint256 amount);
+  event Allocated(address _tokenAddress, uint256 _allocating);
+  event Staked(address indexed user, uint256 amount);
+  event Withdrawn(address indexed user, uint256 amount);
 }
