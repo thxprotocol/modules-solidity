5fe78fa5172128ab1c99206eacc8ba16a6a26292
AlmarMG <almar.gemmel@gmail.com>
2022-06-01 14:28:24 +0200

    blank space deleted

--- test/feeCollector.js
+++ test/feeCollector.js
@@ -85,38 +85,47 @@ describe.only('TimeLockController', function () {
     let test02 = await RewardTOken2.balanceOf(timelockcontroller.address);
     expect(test02).to.equal(200000);
   });
+
   it('Check if contract balance is the expected amount of rewardtoken3', async function () {
     let test03 = await RewardTOken3.balanceOf(timelockcontroller.address);
     expect(test03).to.equal(300000);
   });
+
   it('Check if contract balance is the expected amount of rewardtoken4', async function () {
     let test04 = await RewardTOken4.balanceOf(timelockcontroller.address);
     expect(test04).to.equal(400000);
   });
+
   it('Check if user receives staked thx', async function () {
     let test2 = await stThxToken.balanceOf(admin.getAddress());
     expect(test2).to.equal(2000);
   });
+
   it('Check if contract staked thx balance equals 0', async function () {
     let test3 = await stThxToken.balanceOf(timelockcontroller.address);
     expect(test3).to.equal(0);
   });
+
   it('Check if user can withdraw', async function () {
     await expect(timelockcontroller.withdraw()).to.emit(timelockcontroller, 'Withdrawn');
   });
+
   it('Check if user thx balance equals 10000 after withdrawing', async function () {
     let test4 = await thxToken.balanceOf(admin.getAddress());
     expect(test4).to.equal(10000);
   });
+
   it('Check if user staked thx balance equals 0 after withdrawing', async function () {
     let test5 = await stThxToken.balanceOf(admin.getAddress());
     expect(test5).to.equal(0);
   });
+
   it('Check if user received allocated reward1', async function () {
     await timelockcontroller.payout(admin.getAddress(), RewardTOken1.address);
     let test6 = await RewardTOken1.balanceOf(admin.getAddress());
     expect(test6).to.equal(50000);
   });
+
   it('Check if user received allocated reward2', async function () {
     await timelockcontroller.payout(admin.getAddress(), RewardTOken2.address);
     let test7 = await RewardTOken2.balanceOf(admin.getAddress());
